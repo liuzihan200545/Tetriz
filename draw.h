96f319de572a2df097e2576cc61117d5a56c5d7e
liuzihan <3069522719@qq.com>
2024-06-02 11:54:39 +0800

    绘制主页面

---
+++ draw.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+
+namespace dw
+{
+    void window(int top,int left,int width,int height,std::string title);
+
+
+} // namespace dw
--- main.cpp
+++ main.cpp
@@ -3,24 +3,31 @@
 #include<thread>
 #include<chrono>
 #include"utils.h"
+#include"draw.h"
 using namespace std::chrono;
 
 void init(){
+    tc::clean_screen();
     tc::hide_cursor();
+    dw::window(1,1,9,6,std::string("Hold"));
+    dw::window(1,10,12,22,std::string("Tetriz"));
+    dw::window(7,1,9,16,std::string("Status"));
+    dw::window(19,22,8,4,std::string("Info"));
+    dw::window(1,22,8,18,std::string("Next"));
 }
 
 void loop(){
     int i = 1;
     while (true)
     {
-        tc::move_to(1,1);
+        tc::move_to(8,4);
         std::cout << "FPS:" << ut::fps() << std::endl;
-        tc::move_to(i++%30,10);
+        tc::move_to(9,10);
         tc::set_back_color(15);
         std::cout<<"  "<<std::flush;
         std::this_thread::sleep_for(100ms);
         tc::reset_color();
-        tc::clean_screen();
+        //tc::clean_screen();
     }
 }
 
